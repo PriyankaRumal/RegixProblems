2ab555e02dc770b336c7adff775d9f726412654b
PriyankaRumal <priyankarumal182000@gmail.com>
2022-11-21 17:43:09 +0530

    Solution added

--- UserRegistration/UserValidation.cs
+++ UserRegistration/UserValidation.cs
@@ -7,7 +7,8 @@ namespace UserRegistration
 {
     public class UserValidation
     {
-        public const string First_Name = "^[A-Z][a-zA-z]{2}$";
+        public const string First_Name = "^[A-Z][a-zA-z]{2,}$";
+        //public const string Last_Name=
         public void ValidatateFirstName(string firstName)
         {
             Regex regex = new Regex(First_Name);
